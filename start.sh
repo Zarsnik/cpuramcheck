7d734cce5d5003802e82572290e8fa9945c43ab7
Zarsnik <80834053+Zarsnik@users.noreply.github.com>
2022-07-25 16:08:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp chkd.py /usr/local/sbin/chkd.py
+chmod u+x /usr/local/sbin/chkd.py
 cp chkd.service /etc/systemd/system/chkd.service
 if [[ -e /etc/systemd/system/chkd.service ]] && [[ -e /usr/local/sbin/chkd.py ]]; then
 	systemctl enable chkd.service && systemctl start chkd.service
