7f6fddd2aeaac18ff393ba97a6fd523ec4b0aa9e
Zarsnik <80834053+Zarsnik@users.noreply.github.com>
2022-07-25 14:38:41 +0200

    Update chkd.py

--- chkd.py
+++ chkd.py
@@ -4,15 +4,15 @@ from syslog import syslog as log
 
 def human_read(num, suffix="B"):
   for unit in ["Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
-  if abs(round(num)) < 1024.0:
-    return f"{num:3.1f}{unit}{suffix}"
-  num /= 1024.0
-return f"{num:.1f}Yi{suffix}"
+    if abs(round(num)) < 1024.0:
+      return f"{num:3.1f}{unit}{suffix}"
+    num /= 1024.0
+  return f"{num:.1f}Yi{suffix}"
 
 while True:
   ram=int(bash_outp("vmstat 1 2 | tail -1 | awk '{printf $4}'",shell=True).decode('utf-8'))
   val=(bash_outp("vmstat 1 2 | tail -1 | awk '{printf $14}'",shell=True).decode('utf-8'))
   crit_val=float(75)
-  if float(valeur) > crit_val:
+  if float(val) > crit_val:
     log(2,"["+str(val)+"%] Utilisation CRITIQUE du CPU \n ["+human_read(ram)+"] RAM libre")
     sleep(300)
